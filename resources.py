632b10dc3dafef36f579a36d13a51e2d9eb35a53
Matsaguiar <42449946+Matsaguiar@users.noreply.github.com>
2021-10-26 22:42:02 -0300

    Update resources.py

--- resources.py
+++ resources.py
@@ -11,32 +11,32 @@ class Resources:
     #-retorna 0 se nao foi alocado com sucesso
     def get_resources(self, process):
         free_resources = 1
-        if (process.scanner == 1) and (self.scanner != 0):
+        if (process[6] == 1) and (self.scanner != 0):
             free_resources = 0
-        if (process.modem == 1) and (self.modem != 0):
+        if (process[7] == 1) and (self.modem != 0):
             free_resources = 0
-        if (process.disp_SATA == 1) and (self.disp_SATA[0] != 0):
+        if (process[8] == 1) and (self.disp_SATA[0] != 0):
             free_resources = 0
-        if (process.disp_SATA == 2) and (self.disp_SATA[1] != 0):
+        if (process[8] == 2) and (self.disp_SATA[1] != 0):
             free_resources = 0
-        if (process.impressoras == 1) and (self.impressoras[0] != 0):
+        if (process[5] == 1) and (self.impressoras[0] != 0):
             free_resources = 0
-        if (process.impressoras == 2) and (self.impressoras[1] != 0):
+        if (process[5] == 2) and (self.impressoras[1] != 0):
             free_resources = 0
         
         
         if free_resources == 1:
-            if(process.scanner == 1):
+            if(process[6] == 1):
                 self.scanner = 1
-            if(process.modem == 1):
+            if(process[7] == 1):
                 self.modem = 1
-            if(process.disp_SATA == 1):
+            if(process[8] == 1):
                 self.disp_SATA[0] = 1
-            if(process.disp_SATA == 2):
+            if(process[8] == 2):
                 self.disp_SATA[1] = 1
-            if(process.impressora == 1):
+            if(process[5] == 1):
                 self.impressora[0] = 1
-            if(process.impressora == 2):
+            if(process[5] == 2):
                 self.impressora[1] = 1
         return free_resources
 
