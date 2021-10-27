c31067c5757f87162a2240129082e7c115e94965
Matsaguiar <42449946+Matsaguiar@users.noreply.github.com>
2021-10-26 21:45:03 -0300

    Update resources.py

--- resources.py
+++ resources.py
@@ -1,4 +1,4 @@
-class Resource:
+class Resources:
     #inicializa os recursos
     def __init__(self):
         self.scanner = 0
@@ -9,7 +9,7 @@ class Resource:
     #aloca o recurso para o processo 
     #-retorna 1 se foi alocado com sucesso ou se nada foi pedido
     #-retorna 0 se nao foi alocado com sucesso
-    def get_resource(self, process):
+    def get_resources(self, process):
         free_resources = 1
         if (process.scanner == 1) and (self.scanner != 0):
             free_resources = 0
@@ -25,20 +25,20 @@ class Resource:
             free_resources = 0
         if free_resources == 1:
             if(process.scanner == 1):
-                self.scanner[0] = 1
+                self.scanner = 1
             if(process.impressora == 1):
                 self.impressora[0] = 1
             if(process.impressora == 2):
                 self.impressora[1] = 1
             if(process.modem == 1):
-                self.modem[0] = 1
+                self.modem = 1
             if(process.disp_SATA == 1):
                 self.disp_SATA[0] = 1
             if(process.disp_SATA == 2):
                 self.disp_SATA[1] = 1
         return free_resources
 
-    #libera todos os recursos
+    #libera todos os recursos utilizados pelo processo
     def free_resources(self):
         self.scanner = 0
         self.impressoras = [0, 0]
