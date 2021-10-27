b1b2b96964d4ccf4c0087ba38f0609e072dec0d0
Matsaguiar <42449946+Matsaguiar@users.noreply.github.com>
2021-10-26 21:57:48 -0300

    Update resources.py

--- resources.py
+++ resources.py
@@ -2,9 +2,9 @@ class Resources:
     #inicializa os recursos
     def __init__(self):
         self.scanner = 0
-        self.impressoras = [0, 0]
         self.modem = 0
         self.disp_SATA = [0, 0]
+        self.impressoras = [0, 0]
 
     #aloca o recurso para o processo 
     #-retorna 1 se foi alocado com sucesso ou se nada foi pedido
@@ -13,35 +13,37 @@ class Resources:
         free_resources = 1
         if (process.scanner == 1) and (self.scanner != 0):
             free_resources = 0
-        if (process.impressoras == 1) and (self.impressoras[0] != 0):
-            free_resources = 0
-        if (process.impressoras == 2) and (self.impressoras[1] != 0):
-            free_resources = 0
         if (process.modem == 1) and (self.modem != 0):
             free_resources = 0
         if (process.disp_SATA == 1) and (self.disp_SATA[0] != 0):
             free_resources = 0
         if (process.disp_SATA == 2) and (self.disp_SATA[1] != 0):
             free_resources = 0
+        if (process.impressoras == 1) and (self.impressoras[0] != 0):
+            free_resources = 0
+        if (process.impressoras == 2) and (self.impressoras[1] != 0):
+            free_resources = 0
+        
+        
         if free_resources == 1:
             if(process.scanner == 1):
                 self.scanner = 1
-            if(process.impressora == 1):
-                self.impressora[0] = 1
-            if(process.impressora == 2):
-                self.impressora[1] = 1
             if(process.modem == 1):
                 self.modem = 1
             if(process.disp_SATA == 1):
                 self.disp_SATA[0] = 1
             if(process.disp_SATA == 2):
                 self.disp_SATA[1] = 1
+            if(process.impressora == 1):
+                self.impressora[0] = 1
+            if(process.impressora == 2):
+                self.impressora[1] = 1
         return free_resources
 
     #libera todos os recursos utilizados pelo processo
     def free_resources(self):
         self.scanner = 0
-        self.impressoras = [0, 0]
         self.modem = 0
         self.disp_SATA = [0, 0]
+        self.impressoras = [0, 0]
     
